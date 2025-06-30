943c73ad96ef0c14445d7a31010354202faeac91
Santiago Arcila Guzman <144492957+santiago-arcilag@users.noreply.github.com>
2025-06-29 19:53:46 -0500

    Integrate modules into AppModule and add necessary imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { DriversModule } from './drivers/drivers.module';
+import { PassengersModule } from './passengers/passengers.module';
+import { TripsModule } from './trips/trips.module';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    DriversModule,
+    PassengersModule,
+    TripsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
